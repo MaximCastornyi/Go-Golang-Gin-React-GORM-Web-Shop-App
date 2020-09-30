f875140342e95b66a8de53954d94be0e570db8fd
unknown <nets.devs@gmail.com>
2020-09-30 12:25:36 +0300

    Golang&React

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Main log....")
+	rest.RunAPI(":9090")
+}
---
+++ reacttemplate/src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function About(props) {
+    return (
+        <div className="row mt-5">
+            <div className="col-12 order-lg-1">
+                <h3 className="mb-4">About the Go Music Store</h3>
+                <p>Go music is a modern online msucial instruments store</p>
+
+            </div>
+        </div>);
+}
